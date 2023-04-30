448afa0f6371c79addc4e03a5f35f50d2b101689
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-04-30 21:57:17 +0530

    [COMMIT] Adding reference of features and tests

--- test/test_json2yaml.js
+++ test/test_json2yaml.js
@@ -1,17 +1,11 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 
-const { yamlToJson } = require('../src/json2yaml');
+const { jsonToYaml } = require('../src/yaml2json');
 
-describe('yamlToJson', () => {
-    it('should convert a simple YAML string to a JSON object', () => {
-        const yamlString = `section1:
-  key1: value1
-  key2: value2
-section2:
-  key3: value3
-`;
-        const expectedJson = {
+describe('jsonToYaml', () => {
+    it('should convert a simple JSON object to YAML', () => {
+        const jsonObject = {
             section1: {
                 key1: 'value1',
                 key2: 'value2'
@@ -20,11 +14,29 @@ section2:
                 key3: 'value3'
             }
         };
-        expect(yamlToJson(yamlString)).to.deep.equal(expectedJson);
+        const expectedYaml = `section1:
+  key1: value1
+  key2: value2
+section2:
+  key3: value3
+`;
+        expect(jsonToYaml(jsonObject)).to.equal(expectedYaml);
     });
 
-    it('should convert a YAML string with multiple levels of nesting and multi-line strings to a JSON object', () => {
-        const yamlString = `section1:
+    it('should convert a JSON object with multiple levels of nesting and multi-line strings to YAML', () => {
+        const jsonObject = {
+            section1: {
+                key1: 'value1',
+                key2: {
+                    subkey1: 'value2',
+                    subkey2: 'value3\nwith multiple lines\nand even more lines'
+                }
+            },
+            section2: {
+                key3: 'value4'
+            }
+        };
+        const expectedYaml = `section1:
   key1: value1
   key2:
     subkey1: value2
@@ -34,39 +46,33 @@ section2:
 section2:
   key3: value4
 `;
-        const expectedJson = {
-            section1: {
-                key1: 'value1',
-                key2: {
-                    subkey1: 'value2',
-                    subkey2: 'with multiple lines\nand even more lines'
-                }
-            },
-            section2: {
-                key3: 'value4'
-            }
-        };
-        expect(yamlToJson(yamlString)).to.deep.equal(expectedJson);
+        expect(jsonToYaml(jsonObject)).to.equal(expectedYaml);
     });
 
-    it('should convert a YAML string with empty values to a JSON object', () => {
-        const yamlString = `section1:
-  key1: ''
-  key2: null
-section2: {}
-`;
-        const expectedJson = {
+    it('should convert a JSON object with empty values to YAML', () => {
+        const jsonObject = {
             section1: {
                 key1: '',
                 key2: null
             },
             section2: {}
         };
-        expect(yamlToJson(yamlString)).to.deep.equal(expectedJson);
+        const expectedYaml = `section1:
+  key1: ''
+  key2: null
+section2: {}
+`;
+        expect(jsonToYaml(jsonObject)).to.equal(expectedYaml);
     });
 
-    it('should convert a YAML string with arrays to a JSON object', () => {
-        const yamlString = `section1:
+    it('should convert a JSON object with arrays to YAML', () => {
+        const jsonObject = {
+            section1: {
+                key1: ['value1', 'value2'],
+                key2: [{ 'subkey1': 'value3' }, { 'subkey2': 'value4' }]
+            }
+        };
+        const expectedYaml = `section1:
   key1:
     - value1
     - value2
@@ -74,93 +80,19 @@ section2: {}
     - subkey1: value3
     - subkey2: value4
 `;
-        const expectedJson = {
-            section1: {
-                key1: ['value1', 'value2'],
-                key2: [{ 'subkey1': 'value3' }, { 'subkey2': 'value4' }]
-            }
-        };
-        expect(yamlToJson(yamlString)).to.deep.equal(expectedJson);
+        expect(jsonToYaml(jsonObject)).to.equal(expectedYaml);
     });
 
-    it('should throw an error if the input is not a valid YAML string', () => {
-        const invalidInput = 'not a YAML string';
-        expect(() => yamlToJson(invalidInput)).to.throw();
+    it('should throw an error if the input is not a valid JSON object', () => {
+        const invalidInput = 'not a JSON object';
+        expect(() => jsonToYaml(invalidInput)).to.throw();
     });
 
-    it('should call console.error and return an empty object if the input is an empty string', () => {
+    it('should call console.error and return an empty string if the input is an empty object', () => {
         const consoleErrorSpy = sinon.spy(console, 'error');
-        const emptyString = '';
-        expect(yamlToJson(emptyString)).to.deep.equal({});
-        expect(consoleErrorSpy.calledOnceWith('Empty string passed to yamlToJson')).to.be.true;
+        const emptyObject = {};
+        expect(jsonToYaml(emptyObject)).to.equal('');
+        expect(consoleErrorSpy.calledOnceWith('Empty object passed to jsonToYaml')).to.be.true;
         consoleErrorSpy.restore();
     });
-
-    it('should convert a simple YAML string to a JSON object', () => {
-        const yamlString = `section1:
-  key1: value1
-  key2: value2
-section2:
-  key3: value3
-`;
-        const expectedJson = {
-            section1: {
-                key1: 'value1',
-                key2: 'value2'
-            },
-            section2: {
-                key3: 'value3'
-            }
-        };
-        expect(yamlToJson(yamlString)).to.deep.equal(expectedJson);
-    });
-
-    it('should convert a YAML string with comments to a JSON object', () => {
-        const yamlString = `# This is a comment
-section1:
-  key1: value1 # This is another comment
-  key2: value2
-# This is a comment for section2
-section2:
-  key3: value3
-`;
-        const expectedJson = {
-            section1: {
-                key1: 'value1',
-                key2: 'value2'
-            },
-            section2: {
-                key3: 'value3'
-            }
-        };
-        expect(yamlToJson(yamlString)).to.deep.equal(expectedJson);
-    });
-
-    it('should throw an error if the input is not a valid YAML string', () => {
-        const invalidInput = 'not a YAML string';
-        expect(() => yamlToJson(invalidInput)).to.throw();
-    });
-
-    it('should call console.error and return an empty object if the input is an empty string', () => {
-        const consoleErrorSpy = sinon.spy(console, 'error');
-        const emptyString = '';
-        expect(yamlToJson(emptyString)).to.deep.equal({});
-        expect(consoleErrorSpy.calledOnceWith('Empty string passed to yamlToJson')).to.be.true;
-        consoleErrorSpy.restore();
-    });
-
-    it('should call console.error and return an empty object if the input is a malformed YAML string', () => {
-        const consoleErrorSpy = sinon.spy(console, 'error');
-        const malformedYamlString = `section1:
-  key1: value1
-  key2: value2
-  key3
-section2:
-  key4: value4
-`;
-        expect(yamlToJson(malformedYamlString)).to.deep.equal({});
-        expect(consoleErrorSpy.calledOnceWith('Malformed YAML string passed to yamlToJson')).to.be.true;
-        consoleErrorSpy.restore();
-    });
-
 });
