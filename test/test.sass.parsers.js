6dd2fea21b220fb6e8cb13c7c0231c34ee279415
Ganesh Bhat <ganeshsurfs@gmail.com>
2023-06-07 17:25:55 +0530

    [COMMIT] Adding minor changes to convertors for readme. Adding packages updates from git security dependantbot. Adding test files for the packages

---
+++ test/test.sass.parsers.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: jsparsers
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i jsparsers --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/jsparsers
+ * File: .js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
