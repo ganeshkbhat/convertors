b34c9933cc64ed109b18fb3620452df9033415dd
Ganesh B <ganeshsurfs@gmail.com>
2024-01-13 18:02:29 +0530

    [COMMIT] Adding minor parser and convertor code changes. Minor changes to interfaces. Adding minor changes to repo. Adding Csv parser and interpretor

--- src/csv.js
+++ src/csv.js
@@ -33,37 +33,22 @@ function CsvParser() {
      *
      *
      * @param {*} str
-     * @param {*} options
-     * options: 
-     *      silent: silently fail on parse errors
-     *      source: the path to the file containing Csv. Makes errors and source maps more helpful, by letting them know where code comes from.
-     * 
      * @return {*} 
      * 
      * 
      */
-    this.parse = function (str, options) {
-        // return require("Csv").parse(str, options || { silent: false });
-        return base.Parsers().csv(str, options);
+    this.parse = function (str) {
+        return base.Parsers().csv(str);
     }
 
     /**
      *
      *
      * @param {*} object
-     * @param {*} options
-     * options: 
-     *      indent: the string used to indent the output. Defaults to two spaces.
-     *      compress: omit comments and extraneous whitespace. 
-     *      sourcemap: return a sourcemap along with the csv output. Using the source option of csv.parse is strongly recommended when creating a source map. 
-     *          Specify sourcemap: 'generator' to return the SourceMapGenerator object instead of serializing the source map.
-     *      inputSourcemaps: (enabled by default, specify false to disable) reads any source maps referenced by the input files when generating the output source map. 
-     *          When enabled, file system access may be required for reading the referenced source maps.
      * @return {*} 
      * 
      */
     this.serialize = function (object, options) {
-        // return require("csv").stringify(object, options || { sourcemap: true });
         return base.Convertors().csv(object, options);
     }
 }
@@ -133,8 +118,6 @@ function Csv(object) {
 
 }
 
-
 module.exports.Csv = Csv;
 module.exports.CsvConvertor = CsvConvertor;
 module.exports.CsvParser = CsvParser;
-
