1205a5a77f28403c12f4b0f2baa73afa74258ae1
Ganesh B <ganeshsurfs@gmail.com>
2024-02-05 13:37:46 +0530

    [COMMIT] Adding safe json parser for working with safe conversions

---
+++ src/error.js
@@ -0,0 +1,122 @@
+
+
+// Snippet from 
+// https://www.npmjs.com/package/@walletconnect/jsonrpc-utils?activeTab=code
+
+// ------------ConstantS.js--------------
+
+export const PARSE_ERROR = "PARSE_ERROR";
+export const INVALID_REQUEST = "INVALID_REQUEST";
+export const METHOD_NOT_FOUND = "METHOD_NOT_FOUND";
+export const INVALID_PARAMS = "INVALID_PARAMS";
+export const INTERNAL_ERROR = "INTERNAL_ERROR";
+export const SERVER_ERROR = "SERVER_ERROR";
+export const RESERVED_ERROR_CODES = [];
+export const SERVER_ERROR_CODE_RANGE = [];
+export const STANDARD_ERROR_MAP = {
+    [PARSE_ERROR]: { code: -32700, message: "Parse error" },
+    [INVALID_REQUEST]: { code: -32600, message: "Invalid Request" },
+    [METHOD_NOT_FOUND]: { code: -32601, message: "Method not found" },
+    [INVALID_PARAMS]: { code: -32602, message: "Invalid params" },
+    [INTERNAL_ERROR]: { code: -32603, message: "Internal error" },
+    [SERVER_ERROR]: { code: -32000, message: "Server error" },
+};
+export const DEFAULT_ERROR = SERVER_ERROR;
+
+// -----------ERROR.js-----------
+
+export function isServerErrorCode(code) {
+    return code <= SERVER_ERROR_CODE_RANGE[0] && code >= SERVER_ERROR_CODE_RANGE[1];
+}
+export function isReservedErrorCode(code) {
+    return RESERVED_ERROR_CODES.includes(code);
+}
+export function isValidErrorCode(code) {
+    return typeof code === "number";
+}
+export function getError(type) {
+    if (!Object.keys(STANDARD_ERROR_MAP).includes(type)) {
+        return STANDARD_ERROR_MAP[DEFAULT_ERROR];
+    }
+    return STANDARD_ERROR_MAP[type];
+}
+export function getErrorByCode(code) {
+    const match = Object.values(STANDARD_ERROR_MAP).find(e => e.code === code);
+    if (!match) {
+        return STANDARD_ERROR_MAP[DEFAULT_ERROR];
+    }
+    return match;
+}
+
+
+// -------------FORMAT.js-------------
+
+export function payloadId(entropy = 3) {
+    const date = Date.now() * Math.pow(10, entropy);
+    const extra = Math.floor(Math.random() * Math.pow(10, entropy));
+    return date + extra;
+}
+export function getBigIntRpcId(entropy = 6) {
+    return BigInt(payloadId(entropy));
+}
+
+// export function formatJsonRpcRequest(method, params, id) {
+//     return {
+//         id: id || payloadId(),
+//         jsonrpc: "2.0",
+//         method,
+//         params,
+//     };
+// }
+// export function formatJsonRpcResult(id, result) {
+//     return {
+//         id,
+//         jsonrpc: "2.0",
+//         result,
+//     };
+// }
+// export function formatJsonRpcError(id, error, data) {
+//     return {
+//         id,
+//         jsonrpc: "2.0",
+//         error: formatErrorMessage(error, data),
+//     };
+// }
+// export function formatErrorMessage(error, data) {
+//     if (typeof error === "undefined") {
+//         return getError(INTERNAL_ERROR);
+//     }
+//     if (typeof error === "string") {
+//         error = Object.assign(Object.assign({}, getError(SERVER_ERROR)), { message: error });
+//     }
+//     if (typeof data !== "undefined") {
+//         error.data = data;
+//     }
+//     if (isReservedErrorCode(error.code)) {
+//         error = getErrorByCode(error.code);
+//     }
+//     return error;
+// }
+
+export function isServerErrorCode(code) {
+    return code <= SERVER_ERROR_CODE_RANGE[0] && code >= SERVER_ERROR_CODE_RANGE[1];
+}
+export function isReservedErrorCode(code) {
+    return RESERVED_ERROR_CODES.includes(code);
+}
+export function isValidErrorCode(code) {
+    return typeof code === "number";
+}
+export function getError(type) {
+    if (!Object.keys(STANDARD_ERROR_MAP).includes(type)) {
+        return STANDARD_ERROR_MAP[DEFAULT_ERROR];
+    }
+    return STANDARD_ERROR_MAP[type];
+}
+export function getErrorByCode(code) {
+    const match = Object.values(STANDARD_ERROR_MAP).find(e => e.code === code);
+    if (!match) {
+        return STANDARD_ERROR_MAP[DEFAULT_ERROR];
+    }
+    return match;
+}
---
+++ src/safejson.js
@@ -0,0 +1,45 @@
+/**
+ * JSONStringify & JSONParse inspired by json-with-bigint
+ * source: https://github.com/Ivan-Korolenko/json-with-bigint
+ */
+/* 
+  Function to serialize data to JSON string.
+  Converts all BigInt values to strings with "n" character at the end.
+*/
+/** 
+ * 
+ * https://github.com/WalletConnect/walletconnect-utils/blob/7c6656163001584e898d741bb9173cca4499ae43/misc/safe-json/src/index.ts
+ * 
+ */
+const JSONStringify = data => JSON.stringify(data, (_, value) => typeof value === "bigint" ? value.toString() + "n" : value);
+
+const JSONParse = json => {
+  const numbersBiggerThanMaxInt = /([\[:])?(\d{17,}|(?:[9](?:[1-9]07199254740991|0[1-9]7199254740991|00[8-9]199254740991|007[2-9]99254740991|007199[3-9]54740991|0071992[6-9]4740991|00719925[5-9]740991|007199254[8-9]40991|0071992547[5-9]0991|00719925474[1-9]991|00719925474099[2-9])))([,\}\]])/g;
+  const serializedData = json.replace(numbersBiggerThanMaxInt, "$1\"$2n\"$3");
+  return JSON.parse(serializedData, (_, value) => {
+    const isCustomFormatBigInt = typeof value === "string" && value.match(/^\d+n$/);
+    if (isCustomFormatBigInt)
+      return BigInt(value.substring(0, value.length - 1));
+    return value;
+  });
+};
+
+function safeJsonParse(value) {
+  if (typeof value !== "string") {
+    throw new Error(`Cannot safe json parse value of type ${typeof value}`);
+  }
+  try {
+    return JSONParse(value);
+  }
+  catch (_a) {
+    return value;
+  }
+}
+
+module.exports.safeJsonParse = safeJsonParse;
+
+function safeJsonStringify(value) {
+  return typeof value === "string" ? JSONStringify(safeJsonParse(value)) : JSONStringify(value) || "";
+}
+
+module.exports.safeJsonStringify = safeJsonStringify;
